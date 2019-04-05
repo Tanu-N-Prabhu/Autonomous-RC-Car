9484067083871a896144e04fe0bf7b8401a3474a
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2019-04-04 23:59:01 -0600

    Create src.ino

---
+++ src/src.ino
@@ -0,0 +1,41 @@
+#include <AFMotor.h>
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+
+const int trigPin =9;
+const int echoPin =10;
+
+long duration;
+int distance;
+int turnDelay = 2000; 
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  motor1.setSpeed(150);
+  motor2.setSpeed(200);
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+
+}
+
+void loop()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.034/2;
+
+  Serial.print("Distance:");
+  Serial.println(distance);
+  
+  // This is the spot for the motors to show their magic.
+
+}
