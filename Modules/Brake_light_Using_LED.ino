2d4c45354b8523f83fa25416889b2b297e10f500
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2019-04-09 20:37:05 -0600

    Create Brake_light_Using_LED.ino
    Added code for Brake lights using LED.

---
+++ Modules/Brake_light_Using_LED.ino
@@ -0,0 +1,27 @@
+int led1 = 0;
+int led2 = 1;
+int led3 = 2;
+
+
+void setup() 
+{                
+  pinMode(led1, OUTPUT);   
+  pinMode(led2, OUTPUT);  
+  pinMode(led3, OUTPUT);    
+  
+}
+
+void loop() 
+{
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);   
+  digitalWrite(led3, HIGH);  
+  delay(80); 
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  delay(80);         
+}
+
+Credit: https://www.arduino.cc/en/tutorial/blink
